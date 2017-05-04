cf48688a8b702ad40affc0a8e7db4b29bf4dabd9
LEONID ZHYGAILO <Lee.Morly@gmail.com>
2017-05-04 15:30:10 +0300

    no message

---
+++ Assets/WaysForGhosts.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaysForGhosts : MonoBehaviour {
+	public List<Transform> WayPoints;// динамически изменяемый массив;
+
+	void Start(){//foreach -  посмотреть; 
+		foreach (Transform item in transform) {
+			WayPoints.Add (item);
+		}
+	}
+
+}
---
+++ Assets/Youarewin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Youarewin : MonoBehaviour {
+	public GameObject YouAreWin;
+	public GameObject Rice;
+	public GameObject Music;
+	public GameObject Itself;
+	public AudioClip Tadam;
+	public AudioSource aud;
+	public Text countText;
+
+	// Use this for initialization
+	void Start () {
+//		countText.text = "0";
+//		countText.text = PlayerPrefs.GetInt ("MyRice").ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			}
+		
+
+}
